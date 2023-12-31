81d486bea8783de710314c617d4671a6256a47fa
Kriyan-Tech-Zone <153417340+Kriyan-Tech-Zone@users.noreply.github.com>
2023-12-31 11:30:14 +0530

    Java for loop - continue and break statements
    Please find the code of java for loop
    Continue and break statements code
    While and do while loop code

---
+++ src/JavaForLoop.java
@@ -0,0 +1,34 @@
+public class JavaForLoop {
+    public static void main(String[] args) {
+
+       /* for (int step=1; step<=10; step++){
+            System.out.println("Step::"+step);
+        }*/
+
+   /*     for (int step=10; step>=1; step--){
+            System.out.println("Step::"+step);
+        }
+*/
+        /* loop1:
+         for (int i=1; i<=5; i++){
+            for (int j=1; j<=5; j++){
+                if(i==3 && j==3){
+                    break loop1;
+                }
+                System.out.println(i+","+j);
+            }
+            System.out.println();
+        }*/
+
+       /* String[] studentName = {"Kriyan", "Riya", "Priya"};
+
+        for (String name: studentName) {
+            System.out.println("Name::"+name);
+        }*/
+
+        /*for(;;){
+            System.out.println("infinate for loop");
+        }*/
+
+    }
+}
